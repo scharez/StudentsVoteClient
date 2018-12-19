f2136b6fb0cc342d8088fdc0bdb7ff7003f80196
Stefan Scharinger <stefanscharinger@scharez.at>
2018-12-19 15:12:57 +0100

    LOL

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -32,6 +32,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {AppComponent} from '../app.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 
 @NgModule({
