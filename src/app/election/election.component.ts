75b8bba82a79ebb53dcca8e0e01a862168da36d7
christinasperrer <ChristinaSperrer>
2019-01-16 15:22:43 +0100

    christina 16.01.2018 15:22

--- src/app/election/election.component.ts
+++ src/app/election/election.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Kandidat} from '../Kandidat';
+import {forEachComment} from 'tslint';
 
 @Component({
   selector: 'app-election',
@@ -11,10 +12,22 @@ export class ElectionComponent implements OnInit {
   kandidats: Kandidat[] = [];
   tests: String[] = ['Martin Mayr', 'Markus Berger', 'Max Mustermann', 'Florentina Gruber', 'Melanie Leitner', 'Ernst Lutzky'];
 
+  buttonValue: number[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getValue(getI: number): boolean {
+    let trueFalse = false;
+    for (let i = 0; i < this.tests.length; i++) {
+      if (this.buttonValue[getI] === this.buttonValue[i] && this.buttonValue[getI] !== this.buttonValue[getI]) {
+        trueFalse = true;
+      }
+
+    }
+    return trueFalse;
   }
 
 }
