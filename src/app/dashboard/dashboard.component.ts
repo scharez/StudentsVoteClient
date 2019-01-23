c23e1a7c0cde6b9e9abd107c3c89e1dbfb734732
fionafeichtner <fionafeichtner@gmail.com>
2019-01-23 15:01:09 +0100

    23.1 15:01

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {Kandidat} from '../Kandidat';
+import { Router } from '@angular/router';
+
 
 export interface Class {
   value: string;
@@ -16,14 +16,27 @@ export interface Class {
 export class DashboardComponent implements OnInit {
 
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
   }
 
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  goToDashboard() {
+    this.router.navigate(['dashboard']);
+  }
 
+  goToCreate() {
+    this.router.navigate(['dashboard/create']);
+  }
+
+  goToElect() {
+    this.router.navigate(['dashboard/elect']);
+  }
+
+  goToProfil() {
+    this.router.navigate(['dashboard/profilsettings']);
+  }
 
 
 }
