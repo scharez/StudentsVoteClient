5c9e519ccaccf8ab276b85e8ff472638a20b4430
fionafeichtner <fionafeichtner@gmail.com>
2019-03-14 09:16:00 +0100

    08:25 14.3 für max und markus schnittstellen

--- src/app/dashboard/create/create-candidate/create-candidate.component.ts
+++ src/app/dashboard/create/create-candidate/create-candidate.component.ts
@@ -47,11 +47,12 @@ export class CreateCandidateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.dowloadStudents();
   }
 
   /*Herunterladen von schon eingetragenen Schülern*/
   dowloadStudents() {
-
+    this.httpService.getCandidate().subscribe();
   }
 
   /*Schüler wird mit seinen Daten in Array gespeichert*/
@@ -90,7 +91,7 @@ export class CreateCandidateComponent implements OnInit {
   newStudent(dataString) {
     this.httpService.insert(dataString).subscribe();
     console.log(dataString);
-  }
+  }lalala
 
 
   /*File Upload*/
