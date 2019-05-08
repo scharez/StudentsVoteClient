9ddffb266a6869fb0f4e91b4153745b521cee204
christinasperrer <ChristinaSperrer>
2019-05-08 11:29:58 +0200

    christina 08.05.2019 11:10
    Pop up geht ALLES!
    Election Ordner

--- src/app/election/chooseYourClassComponent.ts
+++ src/app/election/chooseYourClassComponent.ts
@@ -10,23 +10,31 @@ import {Router} from '@angular/router';
 export class ChooseYourClassComponent {
 
   classes: string[] = ['1AHITM', '1BHITM', '2AHITM', '2BHITM', '3AHITM', '3BHITM', '4AHTIM', '4BHITM', '5AHITM'];
-
+  sClass: String = '';
 
   constructor(
     public dialogRef: MatDialogRef<ChooseYourClassComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ClassData,
     private router: Router
   ) {
+    dialogRef.disableClose = true;
   }
 
   onNoClick(): void {
     this.dialogRef.close();
     this.router.navigate(['login']);
 
+  }
 
 
-
-
+  onClick() {
+    console.log(this.sClass);
+    if (this.sClass === '') {
+      location.reload();
+      console.log("ich war hier");
+    } else {
+      this.dialogRef.close();
+    }
   }
 
 }
