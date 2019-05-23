f2a7404b8dd703ecb23ea26963473e4e5db02231
fionafeichtner <fionafeichtner@gmail.com>
2019-05-23 12:49:10 +0200

    22.Mai 3.EH Alles gemerged im election Component nicht ändern!!!

---
+++ src/app/election/finishedComponent.ts
@@ -0,0 +1,37 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {Router} from '@angular/router';
+import {DataService} from '../services/data.service';
+import {HttpService} from '../services/http.service';
+import {ClassData} from '../ClassData';
+
+@Component({
+  selector: 'app-finished',
+  templateUrl: 'finishedComponent.html'
+})
+export class FinishedComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<FinishedComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ClassData,
+    private router: Router,
+    private dataService: DataService,
+    private httpService: HttpService
+  ) {
+
+    this.dialogRef.disableClose = true;
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onClick() {
+    this.dialogRef.close();
+    this.router.navigate(['login']);
+  }
+}
